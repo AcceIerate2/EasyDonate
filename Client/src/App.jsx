a20e77ba4800d43ca3598cfd7cd09a4c99b0a01f
AcceIerate2 <77327200+AcceIerate2@users.noreply.github.com>
2026-08-24 23:44:56 +0200

    apis + simple interface

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,32 +1,126 @@
-import { useState } from 'react'
-import './App.css'
-import SearchResult from './components/searchResult'
+import { useState, useRef } from "react";
+import "./App.css";
+import SearchResult from "./components/searchResult";
 
-function App() {
-    const [results, setResults] = useState([])
-
-    function search(event) {
-      const text = event.target.value 
-      if (text == "") {
-          setResults([])
-        return
-      } 
-
-      setResults([
-        {id: 1, username: text, image: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-9B6BE8CC77C81D8ED7C2249F426DA75D-Png-Background/150/150/AvatarHeadshot/Webp/noFilter", hasProfile: true},
-      ])
+function getIndexById(dict, id) {
+  for (const [i, userData] of dict.entries()) {
+    if (userData.id == id) {
+      return i;
     }
-
-  return (
-    <div id='Container'>
-      <h1 id='Title'>EasyDonation</h1>
-      <textarea id="Username" placeholder="Roblox Username" onInput={search}></textarea>
-
-      <div id='SearchResults'>
-        {results.map(user => (<SearchResult key={user.id} username={user.username} image={user.image} hasProfile={user.hasProfile}/>))}
-      </div>
-    </div>
-  )
+  }
 }
 
-export default App
+function App() {
+  const [results, setResults] = useState([]);
+  const typingTimer = useRef(null);
+
+  function toggleContinueBtnVisibility(toggle) {
+    const button = document.getElementById("continueBtn");
+    button.style.visibility = (toggle && "visible") || "hidden";
+  }
+
+  function search() {
+    const textbox = document.getElementById("Username");
+    const text = textbox.value;
+    console.log(text);
+
+    clearTimeout(typingTimer.current);
+
+    typingTimer.current = setTimeout(function () {
+      if (text == "" || text.length < 3) {
+        setResults([]);
+        toggleContinueBtnVisibility(false);
+        return;
+      }
+
+      var ids = "";
+      fetch("http://localhost:8080/api/getUser?username=" + text, {
+        method: "POST",
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (users) {
+          console.log(users.data, users);
+
+          if (!Array.isArray(users.data)) {
+            return;
+          }
+
+          var newResults = [];
+          users.data.sort(function (a, b) {
+            if (a.name.toLowerCase() == text.toLowerCase()) {
+              return -1;
+            }
+
+            if (b.name.toLowerCase() == text.toLowerCase()) {
+              return -1;
+            }
+
+            return 0;
+          });
+
+          for (const userData of users.data) {
+            const user = {
+              id: userData.id,
+              username: userData.name,
+              hasProfile: true,
+            };
+
+            newResults.push(user);
+            ids = ids + userData.id + ",";
+          }
+
+          var url = "http://localhost:8080/api/fetchAvatarHeadshot?ids=" + ids;
+          fetch(url)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (imageResponses) {
+              for (const userImageData of imageResponses.data) {
+                newResults[
+                  getIndexById(newResults, userImageData.targetId)
+                ].image = userImageData.imageUrl;
+                setResults(newResults);
+              }
+            })
+            .catch(function (error) {
+              console.error("Failed To Fetch ImageUrl", error);
+            });
+        })
+        .catch(console.warn);
+    }, 400);
+  }
+
+  function continueToNext() {
+    return 0;
+  }
+
+  return (
+    <div id="Container">
+      <h1 id="Title">Select A Recipient</h1>
+      <textarea
+        id="Username"
+        placeholder="Roblox Username"
+        onChange={search}
+      ></textarea>
+
+      <button id="continueBtn" onClick={continueToNext}>
+        Continue
+      </button>
+
+      <div id="SearchResults">
+        {results.map((user) => (
+          <SearchResult
+            key={user.id}
+            username={user.username}
+            image={user.image}
+            hasProfile={user.hasProfile}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- Client/src/components/searchResult.jsx
+++ Client/src/components/searchResult.jsx
@@ -1,12 +1,17 @@
-function SearchResult({username, image, hasProfile}) {
-    console.log(username, image)
-    return (
-        <button id='SearchResultBtn'>
-            <img id='SearchResultPfp' src={image}></img>
-            <p id='SearchResultUsername'>{username}</p>
-            <p id="SignedStatus">{hasProfile ? "Custom Donations" : "Game Passes only"}</p>
-        </button>
-    )
+function SearchResult({ username, image, hasProfile }) {
+  function onClicked() {
+    console.log("Clicked");
+  }
+
+  return (
+    <button onClick={onClicked} id="SearchResultBtn">
+      <img id="SearchResultPfp" src={image}></img>
+      <p id="SearchResultUsername">{username}</p>
+      <p id="SignedStatus">
+        {hasProfile ? "Custom Donations" : "Game Passes only"}
+      </p>
+    </button>
+  );
 }
 
-export default SearchResult
+export default SearchResult;
